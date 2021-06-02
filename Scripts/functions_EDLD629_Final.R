800f889217d3fe386fae72a81de3c96690de3a50
Christopher Loan <christopher.m.loan@gmail.com>
2021-06-02 16:35:39 -0700

    almost done

--- Scripts/functions_EDLD629_Final.R
+++ Scripts/functions_EDLD629_Final.R
@@ -154,10 +154,10 @@ se <- function(vector){
   if (sum(is.na(vector)) > 0) {
     vector <- na.omit(vector)
     message('Missing')
-    sd(vector)/sqrt(length(vector))
+    sd(vector, na.rm = T)/sqrt(length(vector))
   }
   else{
-  sd(vector)/sqrt(length(vector))
+  sd(vector, na.rm = T)/sqrt(length(vector))
   }
 }
 
@@ -218,45 +218,130 @@ se_of_moderation <- function(glmertree_mod){
   temp_list 
 }
 
+##### function to make many data sets as one list #####
+
+
+
+make_many_df <- function(
+  intx_values, 
+  num_df, 
+  n_range, 
+  j_range, 
+  intercept_lv1, 
+  main_x, 
+  main_z,
+  residual_var_sd_lv1,
+  random_int_mean_lv2, 
+  random_int_sd_lv2
+  ){
+  
+  
+  all_sims <- list(rep(NA, length(intx_values)))
+  
+  ns_param <- 
+    new_quant_param(
+      type = "integer",
+      range = n_range,
+      inclusive = c(TRUE, TRUE),
+      trans = NULL,
+      label = c(ns_param = "number of level 1 units"),
+      finalize = NULL
+    )
+  
+  
+  js_param <- 
+    new_quant_param(
+      type = "integer",
+      range = j_range,
+      inclusive = c(TRUE, TRUE),
+      trans = NULL,
+      label = c(js_param = "number of level 2 units"),
+      finalize = NULL
+    )
+  
+  parameter_grid <- 
+    grid_max_entropy(
+      ns_param, 
+      js_param, 
+      size = num_df,
+      original = FALSE)
+  
+  for (i in seq_along(intx_values)){
+    
+    intx_value <- intx_values[i]
+    
+    sim_dat <-
+      pmap(
+        list(
+          parameter_grid$ns_param, 
+          parameter_grid$js_param
+          #parameter_grid$intx_param
+        ), 
+        ~simulate_two_level_interaction(
+          n = ..1, 
+          j = ..2, 
+          intercept_lv1 = 10.00, 
+          interaction = intx_value,
+          main_x = 1.25, 
+          main_z = 1.00,
+          residual_var_sd_lv1 = 6.00,
+          random_int_mean_lv2 = 5, 
+          random_int_sd_lv2 = 2.00, 
+          start_seed = 123
+        )
+      )
+    
+    all_sims[[i]] <- sim_dat
+    
+  }
+  
+  names(all_sims) <- intx_values
+  
+  return(all_sims)
+}
+
+
 ##### function to get moderation estimate ####
 
-### the mean of satterwaithes standard error method here is a 
-### PLACEHOLDER until I get the satterwaithe and bootstrap
+### the se estimate here is not right. the project uses the standard error
+### of the estimated average moderation effect. This would be useful
+### to give the option if I publish this.
+
 get_glmertree_moderation <- 
-  function(glmertree_mod, which_variable, mean_only = T, return_all_differences = F){
+  function(
+    glmertree_mod, 
+    which_variable, 
+    mean_only = T, 
+    return_all_differences = F
+    ){
     
     fixed_effects <- coef(glmertree_mod)[,which_variable]
     
     names(fixed_effects) <- NULL
-    
-    test_tibble <- 
-      data.frame(
-        fixed_effect = fixed_effects, 
-        lagged = lag(fixed_effects)
-      )
+    # 
+    # test_tibble <- 
+    #   data.frame(
+    #     fixed_effect = fixed_effects, 
+    #     lagged = lag(fixed_effects)
+    #   )
     ### could use diff() instead of this method, too ###
     ## diff() gives differences between this and lagged automatically
-    differences_in_slopes <-
-      unlist(pmap(
-        .l = list(
-          test_tibble[-1, 1], 
-          test_tibble[-1, 2]), 
-        .f = ~{
-          abs(abs(..1) - abs(..2))
-        }
-      ))
+    differences_in_slopes <- diff(fixed_effects)
     
-    if (mean_only == T){return(mean(differences_in_slopes))}
+    if (mean_only == T){return(mean(differences_in_slopes, na.rm = T))}
     
     output1 <- 
       data.frame(
-        average_moderation = mean(differences_in_slopes), 
-        average_se_mod = mean(se_of_moderation(glmertree_mod = glmertree_mod))
+        average_moderation = mean(differences_in_slopes, na.rm = T), 
+        average_se_mod = 
+          mean(
+            se_of_moderation(glmertree_mod = glmertree_mod), na.rm = T
+            )
       )
     
     output2<- 
       list(
-        average_moderation = mean(differences_in_slopes), 
+        average_moderation = mean(differences_in_slopes, na.rm = T), 
         diff_in_slopes = differences_in_slopes, 
         se_moderation = se_of_moderation(glmertree_mod = glmertree_mod)
       )
@@ -272,7 +357,7 @@ plot_fits <- function(df){
   
   df %>% 
     group_by(model, metric) %>% 
-    mutate(avg_perf = mean(performance)) %>% 
+    mutate(avg_perf = mean(performance, na.rm = T)) %>% 
     ungroup() %>% 
     ggplot(
       aes(x = performance)
@@ -284,7 +369,9 @@ plot_fits <- function(df){
     geom_vline(aes(xintercept = avg_perf, color = model)) +
     facet_wrap(~metric, scales = 'free') +
     theme_minimal() +
-    theme(axis.text.x = element_text(angle = 30)) 
+    theme(
+      axis.text.x = element_text(angle = 30), 
+      legend.position = 'bottom')
   
 }
 
@@ -296,7 +383,7 @@ plot_intx_ests <- function(df, intx_value){
   
   df %>% 
     group_by(type) %>% 
-    mutate(average_effect = mean(estimate)) %>% 
+    mutate(average_effect = mean(estimate, na.rm = T)) %>% 
     ungroup() %>% 
     ggplot(aes(estimate, fill = type)) + 
     geom_density(alpha = 0.4) +
@@ -309,7 +396,8 @@ plot_intx_ests <- function(df, intx_value){
       lty = 2, 
       size = 2
     ) + 
-    theme_minimal() 
+    theme_minimal() + 
+    theme(legend.position = 'bottom')
 }
 
 
@@ -335,7 +423,7 @@ compare_mods <- function(sim_dat, intx_value){
         data = sim_dat[[i]], 
         formula = 
           score ~ 
-          x_lv1 |
+          x_lv1 + z_lv1 |
           (1 | scid) | 
           z_lv1, 
         cluster = scid, 
@@ -369,7 +457,15 @@ compare_mods <- function(sim_dat, intx_value){
     glmertree_rmse[i] <- rmse(temp_tree)
     lmer_rmse[i] <- rmse(temp_lmer)
     
-    print(paste0('iteration #', i, ' complete', sep = ''))
+    print(
+      paste0('iteration #', 
+             i, 
+             ' of ', 
+             length(sim_dat), 
+             ' complete for interaction = ', 
+             intx_value,
+             sep = '')
+      )
   }
   
   fits <- 
@@ -401,40 +497,112 @@ compare_mods <- function(sim_dat, intx_value){
     fits %>% 
     group_by(model, metric) %>% 
     summarize(
-      average_performance = mean(performance), 
-      se_performance = sd(performance)/sqrt(n())
+      average_performance = mean(performance, na.rm = T), 
+      se_performance = sd(performance, na.rm = T)/sqrt(n())
     ) %>% 
-    ungroup()
+    ungroup() %>% 
+    suppressMessages()
   
   intx_ests <- 
     tibble(
       index = rep(1:length(sim_dat), 2), 
       type = c(
-        rep('mod_glmertree', length(sim_dat)), 
-        rep('mod_lmer', length(sim_dat))
+        rep('lmer', length(sim_dat)),
+        rep('tree', length(sim_dat))
       ), 
       estimate = 
-        c(average_glmertree_moderation, average_lmer_moderation)
+        c(average_lmer_moderation, average_glmertree_moderation)
     )
   
   intx_summary <-
     intx_ests %>% 
     group_by(type) %>% 
     summarize(
-      average_estimate = mean(estimate), 
-      se_estimate = sd(estimate)/sqrt(n())
+      average_estimate = mean(estimate, na.rm = T), 
+      se_estimate = sd(estimate, na.rm = T)/sqrt(n())
     ) %>% 
-    ungroup()
+    ungroup() %>% 
+    suppressMessages()
   
   output <- list(
     interaction_summary = intx_summary, 
     fits_summary = fits_summary,
     interactions = intx_ests,
     fits = fits,
-    plot_fits = plot_fits(fits), 
+    plot_fits = plot_fits(fits) + 
+      labs(caption = paste0('interaction value = ', intx_value)), 
     plot_intx_ests = 
-      plot_intx_ests(intx_ests, intx_value))
+      plot_intx_ests(intx_ests, intx_value) + 
+      labs(caption = paste0('interaction value = ', intx_value)))
   
   return(output)
-  
 }
+
+##### make a list with all results you want #####
+
+make_final_results <- 
+  function(
+    intx_values, 
+    num_df, 
+    n_range, 
+    j_range, 
+    intercept_lv1, 
+    main_x, 
+    main_z,
+    residual_var_sd_lv1,
+    random_int_mean_lv2, 
+    random_int_sd_lv2
+  ){
+    map2(.x = 
+           make_many_df(
+             intx_values = intx_values, 
+             num_df = num_df, 
+             n_range = n_range, 
+             j_range = j_range, 
+             intercept_lv1 = intercept_lv1, 
+             main_x = main_x, 
+             main_z = main_z,
+             residual_var_sd_lv1 = residual_var_sd_lv1,
+             random_int_mean_lv2 = random_int_mean_lv2, 
+             random_int_sd_lv2 = random_int_sd_lv2), 
+         .y = intx_values, 
+         ~compare_mods(.x, .y)
+    )
+  }
+
+##### helper functions to extract pieces of the list made make_final_results():
+
+extract_sim_ests <- 
+function(full_list){map(full_list, ~.x[[1]])}
+
+extract_sim_perfomance <- 
+  function(full_list){map(full_list, ~.x[[2]])}
+
+extract_sim_fit_plots <- 
+  function(full_list){map(full_list, ~.x[[5]])}
+
+extract_sim_ests_density <- 
+  function(full_list){map(full_list, ~.x[[6]])}
+
+###### function to create 95% credible (?) intervals #####
+## this especially could be improved
+c_intervals <- 
+  function(
+    full_list, 
+    which_model, 
+    intx_level,
+    lower_level = 0.025, 
+    upper_level = 0.975
+  ){
+    
+    if (is.character(which_model) != T | is.character(intx_level) != T){
+      warning('`which_model` & `intx_level` must be character')
+    }
+    vec <- 
+      full_list[[intx_level]][['interactions']] %>% 
+      filter(type == which_model) %>% 
+      pull(estimate)
+    
+    quantile(vec, probs = c(lower_level, upper_level), na.rm = T)
+    
+  }
